1219dd9d76f02f08906bf1913d79ca4bc0244cd1
Valarmathy <valarmathy.ramachandran@outlook.com>
2022-04-11 21:51:14 +0530

    [Valarmathy]:Added task master table and rendered the template using flask

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 from flask import Flask, render_template
+from mongoengine import document, StringField, IntField, Document, connect
+from datetime import datetime
+from flask_restful import Resource, Api
+
+connect(host='mongodb://127.0.0.1:27017/flask')
 
 app = Flask(__name__)
 
 
-@app.route('/')
+class Todo(Document):
+    id = IntField()
+    content = StringField()
+    date_created = datetime.utcnow()
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
     return render_template('index.html')
 
