0fdc5b19df95480f267346f06ea73bb332b1a9e0
Valarmathy <valarmathy.ramachandran@outlook.com>
2022-04-12 20:41:02 +0530

    [Valarmathy]:refactored code to display ui of task master using jinja template

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template
-from mongoengine import document, StringField, IntField, Document, connect
+import document as document
+from flask import Flask, render_template, request, redirect
+import pymongo
 from datetime import datetime
-from flask_restful import Resource, Api
 
-connect(host='mongodb://127.0.0.1:27017/flask')
+from mongoengine import IntField, StringField, Document
 
 app = Flask(__name__)
+uri = 'mongodb://127.0.0.1:27017/flask'
+client = pymongo.MongoClient(uri)
+db = client['flask']
 
 
 class Todo(Document):
-    id = IntField()
-    content = StringField()
+    query = None
+    id = IntField
+    content = StringField
     date_created = datetime.utcnow()
 
     def __repr__(self):
@@ -19,7 +23,49 @@ class Todo(Document):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+
+    else:
+        # tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html')
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue updating your task'
+
+    else:
+        return render_template('update.html', task=task)
 
 
 if __name__ == "__main__":
